2b4a9917c32a869ac9d28f2315fb16635d5b65a0
Claire Peuz <clairepeuz@yahoo.fr>
2021-11-09 22:51:54 +0000

    Player's inventory

---
+++ Unity Team Commercial Project/Assets/Scripts/Inventory/ItemObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+    public string description;
+}
+
+public enum ItemType
+{
+    Key,
+    Heart,
+    Default
+}
