47ef42012e469bf4302ace97c3035d8bf7076490
garethS-debug <garethjswarte@gmail.com>
2021-11-16 01:38:14 +0000

    Work on the inventory items

--- Unity Team Commercial Project/Assets/Scripts/Inventory/Item.cs
+++ Unity Team Commercial Project/Assets/Scripts/Inventory/Item.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+
 
 public class Item : MonoBehaviour
 {
     public ItemObject item;
+
+
+
 
 }
--- Unity Team Commercial Project/Assets/Scripts/Player/PlayerInventory.cs
+++ Unity Team Commercial Project/Assets/Scripts/Player/PlayerInventory.cs
@@ -10,20 +10,26 @@ public class PlayerInventory : MonoBehaviour
 
 
     [Header("Photon Settings")]
-    PhotonView PV;
+    PhotonView PV;                                              //Setting up photon view 
+
+    GameObject InventoryItem;                                   //Creating a variable for the inventory Item (to send instructions to) 
 
     public void OnTriggerEnter(Collider other)
     {
         var item = other.GetComponent<Item>();
 
-        PV = this.gameObject.GetComponent<PhotonView>();
+        PV = this.gameObject.GetComponent<PhotonView>();        //Get the photonview on the player
 
-        if (PV.IsMine)
+        if (PV.IsMine)                                          //Only run this script on the owning player who triggered the event
         {
             if (item)
             {
-                inventory.AddItem(item.item, 1);                //Photon 
-                Destroy(other.gameObject);
+                inventory.AddItem(item.item, 1);                    //Photon 
+                // PhotonNetwork.Destroy(other.gameObject);        
+                //Destroy(other.gameObject);
+                InventoryItem = other.gameObject;
+                PhotonView photonView = PhotonView.Get(this);       //Get PhotonView on this gameobject
+                photonView.RPC("DestroyObject", RpcTarget.All);   //Send an RPC call to everyone 
             }
         }
     }
@@ -33,4 +39,13 @@ public class PlayerInventory : MonoBehaviour
         // Clear the player's inventory when they quit
         inventory.Container.Clear();
     }
+
+    [PunRPC]
+    void DestroyObject()                                      //Making sure the object is destroyed on everyones copy
+    {
+        //Destroy(gameObject);
+        Debug.Log("Destroy : " + InventoryItem.name);
+        InventoryItem.gameObject.SetActive(false);
+    }
+
 }
