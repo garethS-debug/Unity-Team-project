57e3e24a1dd00f7dc3618c6fadf4b189879238d0
garethS-debug <garethjswarte@gmail.com>
2021-11-16 05:07:54 +0000

    Revert "Work on the inventory items"
    This reverts commit 47ef42012e469bf4302ace97c3035d8bf7076490.

--- Unity Team Commercial Project/Assets/Scripts/Inventory/Item.cs
+++ Unity Team Commercial Project/Assets/Scripts/Inventory/Item.cs
@@ -1,14 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-
 
 public class Item : MonoBehaviour
 {
     public ItemObject item;
-
-
-
 
 }
